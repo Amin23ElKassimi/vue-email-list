3fc9da157a032cd33461bb9294e4a13334b87302
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-12-12 17:31:46 +0100

    Stampa lista emails

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+// Descrizione:
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+
+
+
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            // Var Dove vado a salvare il valore della risposta ottenuta dal JSON dell'API
+            email:''
+        }
+    },
+
+    methods: {
+
+        // Funzione Get Response da API creata da Boolean per Generare email Random
+        getEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(
+                (response) =>{
+                console.log(response);
+                this.email = response.data.response
+            });
+        }
+
+    },
+
+
+    created() {
+        this.getEmail();
+    },
+
+
+}).mount('#app');
